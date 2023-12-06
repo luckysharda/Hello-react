e4250edf47e66e5f9581f62603fbf164c257ab01
lucky sharda <luckysharda1011@gmail.com>
2023-12-06 12:45:53 +0530

    all

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2
-    </div>
-</div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+//react Element => React Element =>  object=>HtmlElement(render)
 
-ReactElement(object) => HTML(Browser Understand)
+// create using core react
 
+//JSX (transpiled before it reaches the js engine) => transpiling is done by parcel-Babel(package)=> takes the code and converts it into js engine
+// JSX=>rect.createelement => reactElement-JS Object => HTMLElement(render)
 
- */
-}
+//react element
+// const heading = <h1 className="head">luffy is coding</h1>; // create using jsx
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
+//React Component
+//1 -> class based component - OLD way
+//2 -> function based component-modern way
+
+//React function component
+// const HeadingComponent = () =>{
+//   return <h1>luffy functional componenet</h1>
+
+// }
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    whats up react
+  </h1>
+);
+const data = 1000;
+//component composition
+const HeadingComponent2 = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading"> luffy functional componenet</h1>
+  </div>
 );
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello from React"
-// );
-console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+root.render(<HeadingComponent2 />); // a way to render functional components
