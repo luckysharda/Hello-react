5b8e4edf7913a9209787769d9604c1ec14695632
lucky sharda <luckysharda1011@gmail.com>
2024-01-04 23:24:52 +0530

    string

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -26,7 +26,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
       <h4>{costForTwo}</h4>
-      <h4>{sla.deliveryTime} minutes</h4>
+      <h4>{sla?.slaString}</h4>
       <h4>User:{loggedInUser}</h4>
     </div>
   );
